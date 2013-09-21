2af127a2a6bca79878855e9a9b8081734ea4efae
Jack Andrew <jacktandrew@gmail.com>
2013-09-20 19:35:32 -0700

    remove small pictures code

--- script.js
+++ script.js
@@ -3,28 +3,18 @@
 jQuery(function($) {
     var fqlUserId = '109877149095742', // the user id
         fqlAlbumId = '25231',  // the album id
-        fqlQuery = "SELECT src_big, caption, src_small, src_small_height, src_small_width, src FROM photo WHERE owner = " + fqlUserId + " AND aid = '" + fqlUserId + '_' + fqlAlbumId + "'",
+        fqlQuery = "SELECT src_big, caption, src FROM photo WHERE owner = " + fqlUserId + " AND aid = '" + fqlUserId + '_' + fqlAlbumId + "'",
         $lightBox = $('.lightBox');
 
     $.getJSON("https://api.facebook.com/method/fql.query?format=json&callback=?&query=" + escape(fqlQuery), function(data) {
         for (var i = 0; i < data.length; i++) {
 
-
-            var height = data[i].src_small_height,
-                width = data[i].src_small_width,
-                $pic = jQuery(  '<div>' +
+            var $pic = jQuery(  '<div>' +
                                     '<a href="' + data[i].src_big + '">' +
-                                        '<img src="' + data[i].src_small + '" alt="' + data[i].caption + '" />' +
+                                        '<img src="' + data[i] + '" title="' + data[i].caption + '" />' +
                                     '</a>' +
                                 '</div>');
 
-            if (height > 90) {
-                $pic.appendTo('.talls');
-            } else if (height >= 70) {
-                $pic.appendTo('.mids');
-            } else if (height >= 48 && width >= 68) {
-                $pic.appendTo('.shorts')
-            }
 
         }
     })
