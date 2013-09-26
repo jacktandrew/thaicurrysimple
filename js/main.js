c55b6dbe0c98121d93f0009ac4def8da0764cfe8
Jack Andrew <jacktandrew@gmail.com>
2013-09-26 01:09:17 -0700

    make demoable

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+$(function(){
+    function setDayOfWeek() {
+        var todayObject = new Date(),
+            todayNumber = todayObject.getDay(),
+            namesOfDays = {
+                0: 'sunday',
+                1: 'monday',
+                2: 'tuesday',
+                3: 'wednesday',
+                4: 'thursday',
+                5: 'friday',
+                6: 'saturday'
+            },
+            todayName = namesOfDays[todayNumber],
+            $todaySpecial = $('.' + todayName);
 
+        console.log(todayName);
+        $todaySpecial.addClass('active');
+    }
+    setDayOfWeek();
+});
