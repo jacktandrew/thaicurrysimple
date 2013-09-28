6f3f291004227fcd03560ae201a1a41becd50923
Jack Andrew <jacktandrew@gmail.com>
2013-09-28 14:28:39 -0700

    add social links and work on feed

---
+++ js/insta.js
@@ -0,0 +1,99 @@
+// <!-- thaicurrysimple userId = 174329427 -->
+// <!-- thaicurrysimple accessToken = 174329427.467ede5.16893db4c3984ba081d6ced06a26dd73 -->
+
+var feed = new Instafeed({
+    get: 'tagged',
+    tagName: 'thaicurrysimple',
+    resolution: 'standard_resolution',
+    limit: '10',
+    sortby: 'most-recent',
+    // userId: 174329427,
+    accessToken: '174329427.467ede5.16893db4c3984ba081d6ced06a26dd73',
+    template:   '<div id="{{id}}" class="instagram_container">' +
+                    '<div id="{{id}}">' +
+                        '<a href="{{link}}" target="_blank" title="{{caption}} by @{{model.user.username}}" >' +
+                            '<img src="{{image}}" alt="{{caption}} by @{{model.user.username}}" class="instagram_image" />' +
+                        '</a>' +
+                    '</div>' +
+
+                    '<div class="owner instagram_image_meta_item">' +
+
+                            '<a href="http://instagram.com/{{model.user.username}}" target="_blank" title="{{model.user.full_name}}" >' +
+                                '<img src="{{model.user.profile_picture}}" class="owner_profile_picture" />' +
+                                '<p>{{model.user.full_name}}</p>' +
+                            '</a>' +
+                        '</p>' +
+                    '</div>' +
+                    '<div class="caption instagram_image_meta_item">' +
+                        '<p>' +
+                            '{{caption}}' +
+                        '</p>' +
+                    '</div>' +
+                '</div>'
+
+
+    // template:   '<div id="{{id}}" class="instagram_container">' +
+    //                 '<div id="{{id}}">' +
+    //                     '<a href="{{link}}" target="_blank" title="{{caption}} by @{{model.user.username}}" >' +
+    //                         '<img src="{{image}}" alt="{{caption}} by @{{model.user.username}}" class="instagram_image" />' +
+    //                     '</a>' +
+    //                 '</div>' +
+    //                 '<div class="instagram_image_meta">' +
+    //                     '<div class="likes instagram_image_meta_item">' +
+    //                         '<p>' +
+    //                             '<a class="icon-heart" href="{{link}}" target="_blank" title="{{caption}}" >' +
+    //                                 '{{likes}}' +
+    //                             '</a>' +
+    //                         '</p>' +
+    //                     '</div>' +
+    //                     '<div class="comments instagram_image_meta_item">' +
+    //                         '<p>' +
+    //                             '<a class="icon-bubble" href="{{link}}" target="_blank" title="{{caption}}" >' +
+    //                                 '{{comments}}' +
+    //                             '</a>' +
+    //                         '</p>' +
+    //                     '</div>' +
+    //                     '<div class="owner instagram_image_meta_item">' +
+    //                         '<p>' +
+    //                             '<a href="http://instagram.com/{{model.user.username}}" target="_blank" title="{{model.user.full_name}}" >' +
+    //                                 '<img src="{{model.user.profile_picture}}" class="owner_profile_picture" />' +
+    //                                 '{{model.user.full_name}}' +
+    //                             '</a>' +
+    //                         '</p>' +
+    //                     '</div>' +
+    //                     '<div class="caption instagram_image_meta_item">' +
+    //                         '<p>' +
+    //                             '{{caption}}' +
+    //                         '</p>' +
+    //                     '</div>' +
+    //                 '</div>' +
+    //             '</div>'
+});
+
+
+feed.run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/share.js
@@ -0,0 +1,39 @@
+function gotoShare(event) {
+    event.preventDefault();
+    String.prototype.format = function() {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g,
+        function(match, number) {
+            return typeof args[number] != 'undefined' ? args[number] : '{' + number + '}';
+        });
+    };
+
+    var facebookFormat = "http://www.facebook.com/share.php?u={0}",
+        twitterFormat = "http://twitter.com/intent/tweet?text={2}",
+        instagramFormat = "http://instagram.com/thaicurrysimple",
+        messageFormat = "Check out {0}!",
+        fullUrl = window.location.href.replace(/\/$/, ""),
+        pageTitle = document.title,
+        message = messageFormat.format(fullUrl, pageTitle),
+        shareUrl = "";
+
+    switch(event.data.param) {
+        case "Facebook":
+            shareUrl = facebookFormat.format(fullUrl, pageTitle, message);
+            break;
+        case "Twitter":
+            shareUrl = twitterFormat.format(fullUrl, pageTitle, message);
+            break;
+        case "Instagram":
+            shareUrl = twitterFormat.format();
+            break;
+
+    }
+    window.open(shareUrl);
+}
+
+$(function(){
+    $('.icon-facebook').click({param: 'Facebook'}, gotoShare)
+    $('.icon-twitter-2').click({param: 'Twitter'}, gotoShare)
+    // $('.icon-instagram').click({param: 'Instagram'}, gotoShare)
+});
