ab3f7b5adfd5a35059408427b785d0557509fcb3
Jack Andrew <jacktandrew@gmail.com>
2013-10-09 12:35:16 -0700

    get minify to work

---
+++ min/groupsConfig.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Groups configuration for default Minify implementation
+ * @package Minify
+ */
+
+/**
+ * You may wish to use the Minify URI Builder app to suggest
+ * changes. http://yourdomain/min/builder/
+ *
+ * See http://code.google.com/p/minify/wiki/CustomSource for other ideas
+ **/
+
+return array(
+    'css' => array('//css/normalize.min.css', '//css/default.css', '//css/main.css', '//css/icons.css', '//css/menu.css', '//css/insta.css'),
+    'js' => array('//js/vendor/jquery-1.10.1.min.js', '//js/plugins.js', '//js/vendor/instafeed.js', '//js/insta.js', '//js/main.js', '//js/share.js', '//js/fb_photos.js'),
+);
